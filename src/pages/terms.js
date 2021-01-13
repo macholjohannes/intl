e64b58060306f035a849f8a1233757d807ed3ade
eacastel <emilio@aliigo.com>
2021-01-12 17:11:56 -0800

    terms-links-fix-2

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { FormattedMessage, injectIntl, Link } from "gatsby-plugin-intl"
+import { ExternalLink } from 'react-external-link';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
