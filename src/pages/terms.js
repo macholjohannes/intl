973e23aa8b4ef6facd17f63b1e9b92fa5ea9a116
eacastel <emilio@aliigo.com>
2021-01-12 17:08:05 -0800

    terms-links-fix

--- src/pages/terms.js
+++ src/pages/terms.js
@@ -83,9 +83,9 @@ const TermsPage = ({ intl }) => (
           ),
         }} />
 
-      <p><FormattedMessage id="terms-and-conditions-text2" /> <Link to="/Contact" className="text-blue01 underline  font-medium"><FormattedMessage id="terms-and-conditions-text3" /></Link>.</p>
+      <p><FormattedMessage id="terms-and-conditions-text2" /> <Link to="/contact" className="text-blue01 underline  font-medium"><FormattedMessage id="terms-and-conditions-text3" /></Link>.</p>
 
-      <p><span className="font-medium"><FormattedMessage id="terms-and-conditions-text4" /></span> <Link to="https://urldefense.proofpoint.com/v2/url?u=HTTPS-3A__COAG.GOV_OFFICE-2DSECTIONS_CONSUMER-2DPROTECTION_CONSUMER-2DCREDIT-2DUNIT_COLLECTION-2DAGENCY-2DREGULATION_&d=DwMGaQ&c=euGZstcaTDllvimEN8b7jXrwqOf-v5A_CdpgnVfiiMM&r=y47BxePAs_nXrhp7DKQnQ4r-fIXxQ1tV9n5j7M610qLXNP_-ZOKKX1s5SrBRs84t&m=soWPyCBX-sJKTywUQQSqZlr0y4woSpGa2kL16yB6rWs&s=vQ4qTCUjlMXGcCKSxiaUe-3w76IRVjlJU697anvSuzY&e=" className="text-blue01 underline  font-medium"><FormattedMessage id="terms-and-conditions-text5" /></Link></p>
+      <p><span className="font-medium"><FormattedMessage id="terms-and-conditions-text4" /></span> <ExternalLink href="https://urldefense.proofpoint.com/v2/url?u=HTTPS-3A__COAG.GOV_OFFICE-2DSECTIONS_CONSUMER-2DPROTECTION_CONSUMER-2DCREDIT-2DUNIT_COLLECTION-2DAGENCY-2DREGULATION_&d=DwMGaQ&c=euGZstcaTDllvimEN8b7jXrwqOf-v5A_CdpgnVfiiMM&r=y47BxePAs_nXrhp7DKQnQ4r-fIXxQ1tV9n5j7M610qLXNP_-ZOKKX1s5SrBRs84t&m=soWPyCBX-sJKTywUQQSqZlr0y4woSpGa2kL16yB6rWs&s=vQ4qTCUjlMXGcCKSxiaUe-3w76IRVjlJU697anvSuzY&e=" className="text-blue01 underline  font-medium"><FormattedMessage id="terms-and-conditions-text5" /></ExternalLink></p>
 
     </div>
   </Layout>
