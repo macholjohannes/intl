d9af65a475fb4570b9989a45e84691e28c996b9b
eacastel <emilio@aliigo.com>
2021-11-30 12:38:49 -0800

    landing-page-redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`,
     `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-source-filesystem`,
