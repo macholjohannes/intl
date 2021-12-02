5689b4eb45f525bbbe0157a517f66ff499094888
eacastel <emilio@aliigo.com>
2021-12-01 20:09:30 -0800

    landing-page-addtl-fixes-404-to-index

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { Link } from 'gatsby'
 
 const NotFoundPage = () => (
   <Layout>
-    <div className='text-center m-24'>
-      <h1>We are sorry...</h1>
-      <p>
-        We were unable to find the page you requested. Please try again or
-        <Link to='/'>return to the homepage</Link>.
-      </p>
+    <div className='mt-6 md:m-10 md:p-10 m-4 p-4 text-center border border-gray-600 border-1 rounded-lg'>
+      We no longer actively service consumer accounts. Payments scheduled to be
+      processed after November 30th will no longer be processed. A successor
+      relationship manager will contact you soon to arrange your future payments
+      and needs. If you need to pay sooner or if this is an emergency, please
+      call us. Main Line: 303-830-0075 or Toll Free: 1-866-729-3328.
     </div>
   </Layout>
 )
