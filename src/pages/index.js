8d12eb2079f9f6730a6bb2e766e8be2999b02c16
eacastel <emilio@aliigo.com>
2021-12-15 20:23:18 -0800

    landing-page-verbiage-change-3

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,16 +6,13 @@ const IndexPage = () => {
     <Layout>
       <div className='mt-6 md:m-10 md:p-10 m-4 p-4 text-center border border-gray-600 border-1 rounded-lg'>
         All cases are either being transferred to a new law firm, being returned
-        to the creditor, or our client. New counsel is in the pr substituting in
-        as attorney of record in pending Court cases. Written notification of
-        the transfer of pending Court cases and payment arrangements, where
-        applicable, are in the process of being mailed out. If this is an urgent
-        matter requiring immediate attention, we have a team of people reviewing
-        voice messages in real-time at (303) 830-0075 or Toll Free
-        1-866-729-3328. We will re-direct and handle your call promptly. If you
-        have an account with LVNV or Resurgent, please call them directly at
-        (888)-665-0374 and your call will be rerouted. Thank you for your
-        patience through this transition.
+        to the creditor, or our client. New counsel is in the process of
+        substituting in as attorney of record in pending Court cases. Written
+        notification of the transfer pending Court cases and payment
+        arrangements, where applicable, are in the process of being mailed out.
+        If this is an urgent matter you may contact the creditor directly. If
+        you have an account with LVNV or Resurgent, please call them directly at
+        (888) 665-0374. Thank you for your patience through this transition.
       </div>
     </Layout>
   )
